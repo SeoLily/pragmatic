ff27d2fc9b052cbf62b61d4c2183726785232ad2
EunJu Seo <eunju.seo@vntgcorp.com>
2021-07-09 22:53:16 +0900

    django course 45 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -12,34 +12,17 @@ from django.views.generic.list import MultipleObjectMixin
 
 from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
-from accountapp.models import HelloWorld
 from articleapp.models import Article
 
 has_ownership = [account_ownership_required, login_required]
 
-@login_required
-def hello_world(request):
-    if request.user.is_authenticated:
-        if request.method == "POST":
-
-            temp = request.POST.get("hello_world_input")
-
-            new_hello_world = HelloWorld()
-            new_hello_world.text = temp
-            new_hello_world.save()
-
-            hello_world_list = HelloWorld.objects.all()
-
-            return HttpResponseRedirect(reverse('accountapp:hello_world'))
-        else:
-            hello_world_list = HelloWorld.objects.all()
-            return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
+
 
 class AccountDetailView(DetailView, MultipleObjectMixin):
     model = User
@@ -52,6 +35,7 @@ class AccountDetailView(DetailView, MultipleObjectMixin):
         object_list = Article.objects.filter(writer=self.get_object())
         return super(AccountDetailView, self).get_context_data(object_list=object_list, **kwargs)
 
+
 @method_decorator(has_ownership, 'get')
 @method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView):
@@ -60,6 +44,7 @@ class AccountUpdateView(UpdateView):
     form_class = AccountUpdateForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
+
 
 @method_decorator(has_ownership, 'get')
 @method_decorator(has_ownership, 'post')
